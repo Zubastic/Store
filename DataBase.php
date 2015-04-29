852e04d1076cd5af923ca32f72adc81b98541d1d
Zubastic <Zubastic32@gmail.com>
2015-04-29 12:42:25 +0300

    DataBase Fix again

--- DataBase.php
+++ DataBase.php
@@ -57,30 +57,11 @@ class DataBaseWorker {
     private $database = null;
 
     private function _makeQuery($login, $statement) {
-        $this->_getDB();
         return $this->database->query($statement);
-        $this->_getDB();
     }
 
     private function _makeExec($login, $statement) {
-        $this->_getDB();
         return $this->database->exec($statement);
-        $this->_getDB();
-    }
-
-
-// TODO: Передавать логин для проверки привилегий.
-    private function _getDB() {
-        unset($this->database);
-        
-        include('db.conf.php');
-        try {
-            $this->database = new PDO("$dbtype:host=$dbhost;dbname=$dbname", $dbuser, $dbpassword);
-            $this->database->exec("SET NAMES 'utf8'");
-            return $dbase;
-        } catch (PDOException $ex) {
-            echo $ex->getMessage();
-        }
     }
 
     public function __construct() {
@@ -94,14 +75,11 @@ class DataBaseWorker {
     }
 
     public function addItemToCart($login, $itemId) {
-        $this->_getDB();
-        
         $slogin = htmlspecialchars($login);
         $sitemId = htmlspecialchars($itemId);
         
         $usrId = $this->findUser($slogin);
         $this->database->exec("INSERT INTO CustomerCart (Acc_Index, Item_Index, Quantity) VALUES ('$usrId', '$sitemId','1')");
-        $this->_getDB();
     }
     
     public function removeItemFromCart($login, $itemId) {
@@ -114,8 +92,6 @@ class DataBaseWorker {
     }
     
     public function addOrder($login, $order) {
-        $this->_getDB();
-        
         $slogin = htmlspecialchars($login);
 
         $userId = $this->findUser($slogin);
@@ -139,39 +115,27 @@ class DataBaseWorker {
             }
         } catch (PDOException $ex) {
             throw $ex;
-        } finally {
-            $this->_getDB();
         }
     }
 
     public function addUser($login, $pwd) {
-        
-        $this->_getDB();
-        
         $s_login = htmlspecialchars($login);
         $spwd = htmlspecialchars($pwd);
         
         // TODO: Хэшировать.
         $this->database->exec("INSERT INTO Accounts (Login, Password) VALUES ('$s_login','$spwd')");
-        $this->_getDB();
-        
         $uId = $this->findUser($s_login);
         $this->_makeExec($s_login, "INSERT INTO Customers_Address (Acc_Index) VALUES ('$uId')");
     }
 
     public function checkUser($login, $pwd) {
-        $this->_getDB();
-        
         $slogin = htmlspecialchars($login);
         $spwd = htmlspecialchars($pwd);
         $info = $this->database->query("CALL CHECK_USER('$slogin', '$spwd')");
-        $this->_getDB();
         return $info->rowCount() > 0;
     }
 
     public function checkoutCart($login) {
-        $this->_getDB();
-        
         $slogin = htmlspecialchars($login);
         
         $order = new Order();
@@ -181,23 +145,17 @@ class DataBaseWorker {
         
         $this->addOrder($slogin, $order);
         $this->clearCart($slogin);
-        $this->_getDB();
     }
 
     public function clearCart($login) {
-        $this->_getDB();
-        
         $slogin = htmlspecialchars($login);
         $id = $this->findUser($slogin);
-        
+      
         $this->database->exec("CALL CLEAR_CART($id)");
-        $this->_getDB();
     }
     
     // Возвращает массив Item-ов.
     public function findItems($searchQuery) {
-        $this->_getDB();
-        
         if (is_null($searchQuery)) {
             return array();
         }
@@ -216,17 +174,13 @@ class DataBaseWorker {
         foreach($arr as $itemArr) {
             $items[] = new Item($itemArr);
         }
-        
-        $this->_getDB();
         return $items;
     }
 
     public function findUser($login) {
-        $this->_getDB();
-        
         $slogin = htmlspecialchars($login);
         $info = $this->database->query("CALL FIND_USER('$slogin')");
-        $this->_getDB();
+
         if ($info->rowCount() > 0) {
             return $info->fetchAll()[0]['Acc_Index'];
         } else {
@@ -235,8 +189,6 @@ class DataBaseWorker {
     }
 
     public function getCart($login) {
-        $this->_getDB();
-        
         // TODO: Учитывать количество.
         $slogin = htmlspecialchars($login);
         $info = $this->database->query("CALL GET_CART('$slogin')");
@@ -248,45 +200,32 @@ class DataBaseWorker {
             //$item->CountToOrder = $arr['Quantity'];
             $items[] = $item;
         }
-        
-        $this->_getDB();
         return $items;
     }
 
     // Возвращает массив имен категорий.
     public function getCategories() {
-        $this->_getDB();
-        
         $info = $this->database->query("CALL GET_CATEGORIES()");
         
         for ($i = 0; $i < $info->rowCount(); $i++) {
             $result[] = $info->fetchColumn();
         }
-        $this->_getDB();
         return $result;
     }
 
     public function getItem($id) {
-        $this->_getDB();
-        
         $sid = htmlspecialchars($id);
         
         $info = $this->database->query("CALL GET_ITEM($sid)");
         $itemArr = $info->fetchAll()[0];
-        
-        $this->_getDB();
         return new Item($itemArr);
         
     }
 
     public function getOrder($num) {
-        $this->_getDB();
         $snum = htmlspecialchars($num);
-        
-        
         $info = $this->database->query("CALL GET_ORDER($num)");
         //print_r($info);
-        
         
         $order = new Order();
         $order->Number = $snum;
@@ -296,13 +235,10 @@ class DataBaseWorker {
         foreach ($tmp as $item) {
             $order->ItemList[] = $this->getItem($item['Goods_Index']);
         }
-        
-        $this->_getDB();
         return $order;
     }
 
     public function getOrders($login) {
-        $this->_getDB();
         $slogin = htmlspecialchars($login);
         $id = $this->findUser($slogin);
         $info = $this->database->query("CALL GET_ORDERS($id)");
@@ -314,13 +250,10 @@ class DataBaseWorker {
             //print_r($orderNum);
             $orders[] = $this->getOrder($orderNum['Ord_Number']);
         }
-        $this->_getDB();
         return $orders;
     }
 
     public function getAllOrders() {
-        $this->_getDB();
-        
         $query = $this->database->query("SELECT Ord_Number FROM Orders");
         $info = $query->fetchAll();
         
@@ -328,13 +261,10 @@ class DataBaseWorker {
         foreach ($info as $orderNum) {
             $orders[] = $this->getOrder($orderNum[0]);
         }
-        $this->_getDB();
         return $orders;
     }
     
     public function getUserInfo($login) {
-        $this->_getDB();
-        
         $s_login = htmlspecialchars($login);
         $id = $this->findUser($s_login);
         
@@ -345,7 +275,6 @@ class DataBaseWorker {
         
         $query = $this->_makeQuery($s_login, "CALL GET_USER_ADDRESS($id)");
         $info = $query->fetchAll()[0];
-        
         
         $a = new Address();
         $a->Country = $info['Country'];
@@ -366,14 +295,10 @@ class DataBaseWorker {
         
         $userInfo->Address = $addr;
         $userInfo->Contacts = $contacts;
-        
-        $this->_getDB();
         return $userInfo;
     }
 
     public function setUserInfo($login, $userInfo) {
-        $this->_getDB();
-        
         $s_login = htmlspecialchars($login);
         
         $s_name = $userInfo->Name;
@@ -395,7 +320,6 @@ class DataBaseWorker {
             $s_contacts[] = new Contact(htmlspecialchars($value->Type), htmlspecialchars($value->Data));
         }
         
-        
         $q = "UPDATE Accounts SET First_Name='$s_name', Last_Name = '$s_surname', Patronym = '$s_midName', Group_Index = $s_priv WHERE Login='$s_login'";
         $this->_makeExec($s_login, $q);
         
@@ -404,18 +328,11 @@ class DataBaseWorker {
         // Адрес.
         $q = "UPDATE Customers_Address SET Country='$s_addr->Country', City='$s_addr->City', Address='$s_addr->Addr', Additional_Info='$s_addr->AdditionalInfo' WHERE Acc_Index=$usrId";
         $this->_makeExec($s_login, $q);
-        
-        // Радикальненько: удаляем все контакты, а потом заливаем сызнова.
-        $q = "DELETE FROM Customers_Contacts WHERE Acc_Index = $usrId";
-        $this->_makeExec($s_login, $q);
-        
-        
+
         foreach ($s_contacts as $contact) {
-            $q = "INSERT INTO Customers_Contacts (Acc_Index, ContactType, Data) VALUES ('$usrId', '$contact->Type', '$contact->Data')";
+            $q = "UPDATE Customers_Contacts SET Data='$contact->Data' WHERE Acc_Index='$usrId' AND ContactType='$contact->Type'";
             $this->_makeExec($s_login, $q);
         }
-        
-        $this->_getDB();
     }
 
     public function removeItem($login, $id) {
